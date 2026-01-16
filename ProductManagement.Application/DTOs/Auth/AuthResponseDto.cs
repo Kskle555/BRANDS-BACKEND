a0c39150833f93832389f680e8e3e186cdca0ef3
Oğuzhan <oguzhanbilgi35@gmail.com>
2026-01-16 08:06:16 +0300

    Auth DTO'ları eklendi

---
+++ ProductManagement.Application/DTOs/Auth/AuthResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductManagement.Application.DTOs.Auth
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; } // Token ne zaman ölecek?
+        public string Role { get; set; }         // Admin mi User mı?
+        public string FullName { get; set; }
+    }
+}
---
+++ ProductManagement.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using ProductManagement.Application.DTOs.Auth;
+using ProductManagement.Domain.Common;
+using System.Threading.Tasks;
+
+namespace ProductManagement.Application.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<ServiceResponse<AuthResponseDto>> LoginAsync(LoginDto request);
+        Task<ServiceResponse<bool>> RegisterAsync(RegisterDto request);
+    }
+}
